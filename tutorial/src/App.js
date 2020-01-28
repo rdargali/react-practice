01dbdbb37cc32e4271e8294affa38f258302de67
Rawand Dargali <rdargali@yahoo.com>
2020-01-27 23:52:56 -0600

    react tutorial dev ed 1/27/2020

---
+++ tutorial/src/App.js
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+import Tweet from "./Tweet"
+
+function App() {
+
+  const [isRed, setRed] = useState(false)
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count + 1)
+    setRed(!isRed)
+  }
+
+  return(
+    <div className ="app">
+      <h1 className={isRed ? "red" : ""}>Change color</h1>
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
+
+    </div>
+  )
+}
+
+export default App
